c9dd04db20c7694a4495903b4e28f5a7fb13c2d1
Pierre Fenoll <pierrefenoll@gmail.com>
2024-03-22 11:08:13 +0100

    add failing test_matrix + tokio case
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- tests/acceptance_cases/cases_can_be_declared_on_async_methods/src/lib.rs
+++ tests/acceptance_cases/cases_can_be_declared_on_async_methods/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg(test)]
-use test_case::test_case;
+use test_case::{test_case, test_matrix};
 
 #[test_case(100i32 => 100usize)]
 #[tokio::test]
@@ -7,8 +7,21 @@ async fn works_seamlessly_with_tokio(arg: i32) -> usize {
     arg as usize
 }
 
+mod conf {
+    use std::sync::OnceLock;
+    pub static SOME_SETTING: OnceLock<bool> = OnceLock::new();
+}
+
+#[test_matrix([true, false], [1, 2, 3, 5, 8])]
+#[tokio::test]
+async fn test_matrix_with_tokio(b: bool, arg: usize) {
+    conf::SOME_SETTING.set(b).expect("Setting SOME_SETTING");
+    assert_eq!(arg, arg);
+    ()
+}
+
 #[test_case(100i32 => 100usize)]
 #[async_std::test]
 async fn works_seamlessly_with_async_std(arg: i32) -> usize {
-        arg as usize
-    }
+    arg as usize
+}
